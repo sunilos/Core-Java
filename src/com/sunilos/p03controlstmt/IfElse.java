2c867c4091e13f3f20eb6b617dd5124413401d59
SunilOS <sunilos007@gmail.com>
2019-08-08 20:41:38 +0530

    IfElse added

---
+++ src/com/sunilos/p03controlstmt/IfElse.java
@@ -0,0 +1,22 @@
+package com.sunilos.p03controlstmt;
+
+/**
+ * Example class for IF-ELSE control statement
+ * 
+ * @author SunilOS
+ * @version 1.0
+ * @Copyright (c) SunilOS
+ */
+
+public class IfElse {
+
+	public static void main(String[] args) {
+		int price = 100;
+		if (price > 100) {
+			System.out.println("Wow !! I can buy Pizza");
+		} else {
+			System.out.println("Oh !! I can not buy Pizza");
+		}
+	}
+
+}
