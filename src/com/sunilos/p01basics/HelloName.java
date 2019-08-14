bb96d41b3565b9d26985c63f09ec7366f74a0516
PrachiPacharne <52574595+PrachiPacharne@users.noreply.github.com>
2019-08-14 15:07:19 +0530

    Update HelloName.java

--- src/com/sunilos/p01basics/HelloName.java
+++ src/com/sunilos/p01basics/HelloName.java
@@ -1,10 +1,13 @@
 package com.sunilos.p01basics;
 
-/*
- * @author  SunilOS
+/**
+ * A simple program to print runtime argument with Hello.  
+ * 
  * @version 1.0
- * @Copyright (c) SunilOS
- *
+ * @since 16 Nov 2014
+ * @author Sunil Sahu
+ * @Copyright (c) Sunil Sahu
+ * @url www.sunilbooks.com
  */
 
 public class HelloName {
